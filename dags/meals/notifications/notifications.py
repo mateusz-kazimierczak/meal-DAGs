9fda00a1364cf157c9bde9c2e3e6597f71f74537
Mateusz-alicante <mateusz.alicante@gmail.com>
2025-09-07 15:12:46 -0400

    fixed dag

--- dags/meals/notifications/notifications.py
+++ dags/meals/notifications/notifications.py
@@ -6,8 +6,6 @@ import pendulum
 from airflow.sdk import dag, task
 
 from meals.notifications.get_relevant_users.get_relevant_users import get_relevant_users_task
-from meals.notifications.get_relevant_users.generate_notification_objects import generate_notification_objects_task
-
 @dag(
     schedule=None,
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
@@ -20,16 +18,9 @@ def meal_notifications():
     def get_relevant_users():
         users = get_relevant_users_task()
 
-        print("Relevant users:")
-        for user in users:
-            print(f"{user.firstName} ({user.email})")
-
         return users
 
-    def generate_notifications(users):
-        return generate_notification_objects_task(users)
-
-    get_relevant_users() >> generate_notifications()
+    get_relevant_users()
 
 
 dag = meal_notifications()
