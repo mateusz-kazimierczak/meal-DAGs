129761b47a9b75d53a42e5a0deb5612dfe1b7fde
Mateusz-alicante <mateusz.alicante@gmail.com>
2025-09-14 09:15:52 -0400

    updated js

--- dags/meals/notifications/send_emails/index.js
+++ dags/meals/notifications/send_emails/index.js
@@ -1,3 +1,28 @@
 import { Resend } from "resend";
 
 console.log("Resend API Key:", process.env.RESEND_API_KEY);
+
+const send_emails = async (users) => {
+    try {
+      console.log(users)
+    } catch (err) {
+      console.log("Error sending email:", err);
+    }
+};
+
+const read_user_from_json = async () => {
+    try {
+        const data = await fs.promises.readFile('notifications.json', 'utf-8');
+        return JSON.parse(data);
+    } catch (err) {
+        console.log("Error reading user data:", err);
+        return [];
+    }
+};
+
+const main = async () => {
+    const users = await read_user_from_json();
+    await send_emails(users);
+};
+
+main();
