d28213d30154f7bc6619259fd3b2b86e6c868cff
Mateusz-alicante <mateusz.alicante@gmail.com>
2025-11-15 22:30:40 -0500

    fixed query

--- dags/meals/_common/mongo_utils/get_future_meals.py
+++ dags/meals/_common/mongo_utils/get_future_meals.py
@@ -26,7 +26,7 @@ def get_future_meals(date, mongo_conn_id="mongoid", db_name="test", collection_n
     meal_collection = client[db_name][collection_name]
 
     # Get all active users
-    active_users = meal_collection.find({"isActive": True})
+    active_users = meal_collection.find({"active": True})
 
     total_counts = {"B": 0, "L": 0, "S": 0}
 
